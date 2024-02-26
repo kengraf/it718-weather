547bf2a416bca8df1f31c337434bf92bff0453dc
Ken Graf <kengraf57@gmail.com>
2024-02-25 22:19:29 -0500

    Update openweather.py

--- aws/openweather.py
+++ aws/openweather.py
@@ -1,4 +1,5 @@
-# openweather.py
+import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     """
@@ -11,3 +12,35 @@ def lambda_handler(event, context):
         },
         'body': 'Hello, world!'
     }
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+
+def get_secret():
+
+    secret_name = "opeweatherapi-apikey"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
+    return( secret )
+
