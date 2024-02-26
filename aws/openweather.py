be46df4fe38d99107fe053a1db306a138cae3897
Ken Graf <kengraf57@gmail.com>
2024-02-26 10:34:05 -0500

    Update openweather.py

--- aws/openweather.py
+++ aws/openweather.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import urllib
 from botocore.exceptions import ClientError
 
@@ -6,23 +7,25 @@ def lambda_handler(event, context):
     status_code = 200
     response_message = ''
     query_params = event.get('queryStringParameters', {})
-    zipcode = query_params.get('zip')
+    if len(query_params) > 0:
+        zipcode = query_params.get('zip')
     
     if len(zipcode) == 5:
         apikey = get_secret('opeweatherapi-apikey')
         ## construct full URL to invoke OpenWeatherMap service with proper inputs
         baseUrl = 'http://api.openweathermap.org/data/2.5/weather'
         completeUrl = f"{baseUrl}?zip={zipcode}&appid={apikey}"
+        print(completeUrl)
         ## Invoke OpenWeatherMap API and parse response with proper exception handling
         try:
             apiresponse = urllib.request.urlopen(completeUrl)
             response_message = apiresponse.read().decode('utf-8') 
         except urllib.error.HTTPError as e:
             status_code=e.code
-            response_message = "The server couldn't fulfill the request. Error code:" + e.code
+            response_message = e
         except urllib.error.URLError as e:
             status_code=e.code
-            response_message =   "We failed to reach a server. Reason:" + e.reason
+            response_message = e
     else:
         status_code = 400
         response_message = 'URL args required ?zip=XXXXX'
@@ -40,13 +43,12 @@ def get_secret(secret_name):
     client = boto3.client('secretsmanager')
 
     try:
-        get_secret_value_response = client.get_secret_value(SecretIdsecret_name,)
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name,)
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         raise e
 
     secret = get_secret_value_response['SecretString']
-
-    # Your code goes here.
-    return( secret )
+    key = json.loads(secret)['openweatherapikey']
+    return( key )
