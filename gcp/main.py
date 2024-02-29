6cc44b06d7c11dbc907563281ffaca90aa5cd1f8
Ken Graf <kengraf57@gmail.com>
2024-02-29 11:41:57 -0500

    Update main.py

--- gcp/main.py
+++ gcp/main.py
@@ -1,5 +1,6 @@
 import json
 import urllib
+from google.cloud import secretmanager_v1beta1 as secretmanager
 
 def get_weather(request):
     # Extract query parameters from the request
@@ -27,4 +28,15 @@ def get_weather(request):
         return 'URL args required ?zip=XXXXX',400
 
 def get_secret(secret_name):
-    return( '' )
+    project_id = 'unh-it718'
+    
+    # Create the Secret Manager client
+    client = secretmanager.SecretManagerServiceClient()
+    
+    # Build the secret name
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    # Access the secret
+    response = client.access_secret_version(name=name)
+    api_key = response.payload.data.decode('UTF-8')
+
+    return( api_key )
